81b030b639c8f1970a25e616a60e9bf2fb6526ad
Anwesh43 <anweshthecool0@gmail.com>
2020-09-09 01:20:44 +0530

    adding Canvas extension methods

--- halfarcbetweenbarsview/src/main/java/com/anwesh/uiprojects/halfarcbetweenbarsview/HalfArcBetweenBarsView.kt
+++ halfarcbetweenbarsview/src/main/java/com/anwesh/uiprojects/halfarcbetweenbarsview/HalfArcBetweenBarsView.kt
@@ -32,3 +32,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawHalfArcBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val barSize : Float = w / barSizeFactor
+    val r : Float = (w - 2 * barSize) / 2
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(0f, h / 2)
+        drawRect(RectF(w / 2 - barSize, 0f, w / 2, -r * sf1), paint)
+        restore()
+    }
+    drawArc(RectF(-r, h / 2 - r, r, h / 2 + r), start, sweep * sf2, true, paint)
+    restore()
+}
+
+fun Canvas.drawHABBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawHalfArcBar(scale, w, h, paint)
+}
