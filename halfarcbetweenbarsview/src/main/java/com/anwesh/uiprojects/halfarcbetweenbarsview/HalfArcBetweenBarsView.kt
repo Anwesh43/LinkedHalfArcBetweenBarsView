9a2ace688b7356cee3df5912a925f14a6580f93c
Anwesh43 <anweshthecool0@gmail.com>
2020-09-09 01:40:11 +0530

    adding HABBNode class

--- halfarcbetweenbarsview/src/main/java/com/anwesh/uiprojects/halfarcbetweenbarsview/HalfArcBetweenBarsView.kt
+++ halfarcbetweenbarsview/src/main/java/com/anwesh/uiprojects/halfarcbetweenbarsview/HalfArcBetweenBarsView.kt
@@ -121,4 +121,45 @@ class HalfArcBetweenBarsView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HABBNode(var i : Int, val state : State = State()) {
+
+        private var next : HABBNode? = null
+        private var prev : HABBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HABBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHABBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HABBNode {
+            var curr : HABBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
