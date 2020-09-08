a60019a4cb731d040a101103192304ff50799906
Anwesh43 <anweshthecool0@gmail.com>
2020-09-09 01:12:32 +0530

    adding constants and libraries

--- halfarcbetweenbarsview/src/main/java/com/anwesh/uiprojects/halfarcbetweenbarsview/HalfArcBetweenBarsView.kt
+++ halfarcbetweenbarsview/src/main/java/com/anwesh/uiprojects/halfarcbetweenbarsview/HalfArcBetweenBarsView.kt
@@ -3,3 +3,27 @@ package com.anwesh.uiprojects.halfarcbetweenbarsview
 /**
  * Created by anweshmishra on 09/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#4CAF50",
+        "#FF5722",
+        "#2196F3",
+        "#3F51B5"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 3
+val barSizeFactor : Float = 5.2f
+val scGap : Float = 0.02f / parts
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val start : Float = 180f
+val sweep : Float = 180f
